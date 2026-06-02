7a9b486288ccda935d7db65d95ad1d8def9ba364
Gentian <gentianshabani007@gmail.com>
2026-06-02 14:21:12 +0200

    small fix

--- test/obj/Debug/net10.0/test.AssemblyInfo.cs
+++ test/obj/Debug/net10.0/test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b64b774b718abad6a1b142198dc40b28e63620b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
